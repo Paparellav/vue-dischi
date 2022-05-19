93f530ecc95a567af2c4e877ee8c5ca1af3300f0
Paparellav <vitantoniopaparella@gmail.com>
2022-05-19 13:52:03 +0200

    header completed

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="header">
+    <div class="header__logo">
+      <img src="../assets/img/logo-spotify.png" alt="logo-spotify" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../style/common.scss";
+@import "../style/variables.scss";
+
+.header {
+  width: 100%;
+  height: 100px;
+  display: flex;
+  align-items: center;
+  color: $primary-text-color;
+  background-color: $primary-bg-color;
+
+  &__logo {
+    width: 80px;
+    padding-left: 0.5rem;
+    margin-left: 2rem;
+
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
